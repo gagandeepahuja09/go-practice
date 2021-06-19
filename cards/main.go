477ff609e88933a67f1f980b6852f0aa614cf210
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-06-20 00:38:14 +0530

    custom type declarations & receiver functions

--- cards/main.go
+++ cards/main.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-// we can't do shorthand variable initialization outside main
-// because outside main variable can only be declared & not initialized
-
 func main() {
-	// var card string = "Ace of Spades"
-	// this is shorthand syntax + we can let go infer the type on its own.
-	// := is shorthand syntax for initialization.
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	cards := deck{newCard(), "Five of Diamonds"}
+
+	cards.print()
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
