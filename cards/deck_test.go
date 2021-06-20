f12178a96ced28ad1139795861ed143fdf30a4c9
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-06-20 22:15:21 +0530

    running basic tests with go

---
+++ cards/deck_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Expected length of 16 but got length %v", len(d))
+	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expect first card as Ace of Spades but got %v", d[0])
+	}
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expect first card as Four of Clubs but got %v", d[len(d)-1])
+	}
+}
