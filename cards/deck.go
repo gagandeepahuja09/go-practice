88c3aaf5a5d88e7676583abb0ff9e3f6a153a9bb
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-06-20 16:21:05 +0530

    implement deal function using slice range syntax & returning multiple statements

--- cards/deck.go
+++ cards/deck.go
@@ -19,6 +19,10 @@ func newDeck() deck {
 	return cards
 }
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,9 @@ package main
 func main() {
 	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
 }
 
 func newCard() string {
