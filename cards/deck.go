20c1c645b7e91c89cb8c088cecd24f3071a17fd3
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-06-20 17:45:41 +0530

    saving to file using ioutil, tostring conversion(join), type conversion to []byte

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type deck []string
 
@@ -27,4 +31,12 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -2,10 +2,7 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
 
 func newCard() string {
