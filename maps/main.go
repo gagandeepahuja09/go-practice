53fde4b82955b70a6a8f66e5fb271dbb3a467bc0
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-06-28 21:32:43 +0530

    difference between maps and structs

--- maps/main.go
+++ maps/main.go
@@ -1,19 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// colors := map[string]string{
-	// 	"red": "#ff0000",
-	// 	"green": "#00ff00",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+	}
 
 	// var color map[string]string
 
-	colors := make(map[int]string)
+	// colors := make(map[int]string)
 
-	colors[10] = "#ff0000"
-	fmt.Println(colors)
-	delete(colors, 10)
-	fmt.Println(colors)
+	// colors[10] = "#ff0000"
+	printMap(colors)
+	// delete(colors, 10)
+	// fmt.Println(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code of", color, "is", hex)
+	}
 }
