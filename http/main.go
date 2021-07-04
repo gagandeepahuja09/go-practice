86a23b72d04853641751b06d873824a5db013177
gagandeepahuja007 <gagandeepahuja09@gmail.com>
2021-07-04 18:19:49 +0530

    add: logWriter in io.Copy which implements Writer

--- http/main.go
+++ http/main.go
@@ -7,11 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (lw logWriter) Write(b []byte) (int, error) {
+	fmt.Println(string(b))
+	fmt.Println("No. of bytes scanned = ", len(b))
+	return len(b), nil
 }
