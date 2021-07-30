57852b1c91837bc6392f4aa80cd33c26fcdb4a30
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2021-07-30 14:44:35 +0530

    basic exercise for package scoped vars & types & printing methods T & v

---
+++ ninja-levels/ninja-level-1/ex-1.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Variables should have package level scope
+// Do not assign values to these variables.
+// vars declared outside of main have package level scope
+
+// Create your own type with int as the underlying type
+type intRep int
+type intRep2 intRep
+
+var ir intRep = 42
+var ir2 intRep2 = 43
+
+var a int = 42              // 0
+var b string = "James Bond" // ""
+var c bool = true           // false
+
+func main() {
+	// %T will give us the type and %v the value
+	fmt.Printf("%T \t%v \n%T \t%v\n", ir, ir, ir2, ir2)
+	a = 42
+	b = "James Bond"
+	c = true
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x, y, z)
+	s := fmt.Sprintf("The result is \t %v \t %v \t %v", x, y, z)
+	fmt.Println(s)
+}
