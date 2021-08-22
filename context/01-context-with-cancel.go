00dbf9f3ecf7bfa369c94a230a1a5aac7d9e3fc6
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2021-08-22 13:24:34 +0530

    context with cancel

---
+++ context/01-context-with-cancel.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func task(ctx context.Context) {
+	i := 1
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Ending the context")
+			fmt.Println(ctx.Err())
+			time.Sleep(time.Second)
+		default:
+			fmt.Println(i)
+			time.Sleep(time.Second)
+			i++
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	cancelCtx, cancel := context.WithCancel(ctx)
+
+	go task(cancelCtx)
+	time.Sleep(time.Second * 4)
+	cancel()
+	time.Sleep(time.Second * 1)
+}
