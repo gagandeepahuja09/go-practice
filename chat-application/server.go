d12445a788c3df03707545d2be15cf97d6a2964a
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2021-11-05 21:56:13 +0530

    1. change to using templates instead of hardcoding html 2. using package like sync.Once to ensure that the operation is performed only once

--- chat-application/server.go
+++ chat-application/server.go
@@ -3,21 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's chat
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	fileName string
+	once     sync.Once
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(
+			template.ParseFiles(filepath.Join("templates", t.fileName)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{fileName: "chat.html"})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Listen and serve:", err)
