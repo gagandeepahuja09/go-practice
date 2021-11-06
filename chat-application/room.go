639abdb1768aa2b1883449093e80013cc5102b34
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2021-11-06 10:41:13 +0530

    modelling the client using join, leave channel and map of all clients in a room

--- chat-application/room.go
+++ chat-application/room.go
@@ -4,4 +4,16 @@ type room struct {
 	// forward is a channel that will hold incoming messages that should be
 	// forwarded to other clients
 	forward chan []byte
+
+	// join is a channel for clients wishing to join the room
+	join chan *client
+
+	// leave is a channel for client wishing to leave the room
+	leave chan *client
+
+	// clients holds all current clients in this room
+	// if we were to access the map directly, it is possible that 2 goroutines running
+	// concurrently might try to modify the map at the same time resulting in
+	// corrupt memory or an unpredictable state.
+	clients map[*client]bool
 }
