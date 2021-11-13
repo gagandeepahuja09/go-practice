4ea68f21dbc4e0cebd7131231cfdac71bfaa450b
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2021-11-13 19:57:48 +0530

    create auth handler and use it must auth method to wrap any other handler

---
+++ chat-application/auth.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// redirect to login page in this case
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
