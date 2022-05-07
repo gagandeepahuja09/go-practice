a5efa9c6b532e6541104071af6c581f390cf8035
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2022-05-07 11:57:31 +0530

    integrating the transaction log in our web service

--- go-cloud-native/distributed-key-val-store/main.go
+++ go-cloud-native/distributed-key-val-store/main.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+var logger TransactionLogger
+
+// initializeTransactionLog creates the transaction logger file,
+// reads events which are already there in the file and updates the in memory cache
+// accordingly. It then uses the Run method to listen to events and then write to the
+// file.
+func initializeTransactionLog() error {
+	var err error
+	logger, err = NewFileTransactionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("failed to create event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+	e, ok := Event{}, true
+
+	// ok will be false if the channel is closed.
+	for ok && err == nil {
+		select {
+		case err, ok = <-errors: // Retrieve any errors
+		case e, ok = <-events:
+			switch e.EventType {
+			case EventDelete:
+				err = Delete(e.Key)
+			case EventPut:
+				err = Put(e.Key, e.Value)
+			}
+		}
+	}
+
+	logger.Run()
+
+	return err
+}
 
 func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -30,6 +66,7 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 			http.StatusInternalServerError)
 		return
 	}
+	logger.WritePut(key, string(value))
 
 	w.WriteHeader(http.StatusCreated)
 }
@@ -49,6 +86,8 @@ func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	initializeTransactionLog()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
 	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
